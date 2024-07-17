0101832e93e815ccb236cb430de9a4a1ff457310
Ronald Shumery <ronaldshumery@gmail.com>
2024-07-17 15:03:29 -0400

    probando 2

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/api/ip', (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-    res.json({ ip });
+// Middleware para configurar CSP
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-eval'");
+    next();
 });
 
+// Rutas y configuración de tu aplicación
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Puerto y servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
